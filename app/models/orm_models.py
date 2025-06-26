ae497e37dd4d7461df0be057ddf275af255c51e1
gaetanodigrazia <gaetano.digrazia@bitrock.it>
2025-06-26 18:25:54 +0200

    Changed id to uuid

--- app/models/orm_models.py
+++ app/models/orm_models.py
@@ -36,7 +36,7 @@ class SchemaNutrizionale(Base):
 
 class Utente(Base):
     __tablename__ = "utenti"
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     username = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
     keysession = Column(String)
